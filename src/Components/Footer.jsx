ac2bc1286a2e49bc1e6aef6643b747423fbecac4
galit.moss <galitmoss@me.com>
2023-08-13 12:13:49 +1000

    Footer wording

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import { Text } from "@chakra-ui/react";
 export default function Footer() {
   return (
     <Text fontSize="12px" m="30px">
-      Can I BRAD This was created by Galit Moss
+      Created by Galit Moss & powered by Supabase ⚡️
     </Text>
   );
 }
