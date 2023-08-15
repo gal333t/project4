c36a9bbdf1970a4c69618fe36a6200e446590977
galit.moss <galitmoss@me.com>
2023-08-15 20:10:51 +1000

    troubleshooting the Login errors for Magic Code, error handling, setting Context

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import supabase from "../supabase.js";
 import {
   Card,
@@ -8,35 +8,56 @@ import {
   useToast,
   useColorModeValue,
 } from "@chakra-ui/react";
-import BradItem from "./BradItem.jsx";
+import { SessionContext } from "./SessionContext";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
   const [email, setEmail] = useState("");
   const [magicCode, setMagicCode] = useState("");
   const [submitted, setSubmitted] = useState(false);
+  const { setUsername, setUserScore } = useContext(SessionContext);
 
-  const isError = magicCode.length !== 6;
-
+  const navigate = useNavigate();
   const toast = useToast();
+
   const white = useColorModeValue("white", "white");
   const black = useColorModeValue("black", "black");
 
   async function determineUser(emailInput) {
     // need to change method to not error if email doesn't exist
+
+    // emailInput = "email address"
+    // determine if user exists first
+    // if email does not already exist =>
+
+    // if the below is NULL then user does not already exist, else user does exist
     const { data } = await supabase
       .from("Users")
-      .select("email")
+      .select("email, score")
       .eq("email", emailInput);
-    console.log(data);
+
     if (data.length !== 0) {
       console.log("email matches one with account already");
+      setUsername(data[0].username);
+      setUserScore(data[0].score);
     } else {
       // not working to add new User to table Users.. trying const {data} to match docs
       const { data } = await supabase
         .from("Users")
         .insert({ email: emailInput });
+      console.log(data);
+      navigate("/username");
       console.log("user does not exist, need to create new user in table here");
     }
+  }
+
+  async function dertermineUsername() {
+    // to run once MagicCode is Successful
+    // add email input from { data }
+    // probably can reuse the above function and replace with one that runs at Magic Code
+    // if username already exists
+    // navigate("/")
+    // else { navigate("/username")} => setup navigate("/") in Username
   }
 
   return (
@@ -84,7 +105,6 @@ export default function Login() {
                       duration: 3000,
                       isClosable: true,
                     });
-                    determineUser(email);
                   }
                 }}
               >
@@ -115,15 +135,8 @@ export default function Login() {
                 color={"#66a8ba"}
                 bg={white}
                 _hover={{ opacity: "80%" }}
-                onClick={() => {
-                  if (magicCode.length <= 0) {
-                    toast({
-                      description: "Magic Code cannot be blank",
-                      status: "error",
-                      duration: 3000,
-                      isClosable: true,
-                    });
-                  } else if (magicCode.length !== 6) {
+                onClick={async () => {
+                  if (magicCode.length !== 6) {
                     toast({
                       description: "Magic Code must be 6 numbers",
                       status: "error",
@@ -131,11 +144,22 @@ export default function Login() {
                       isClosable: true,
                     });
                   } else {
-                    supabase.auth.verifyOtp({
+                    const { data, error } = await supabase.auth.verifyOtp({
                       type: "email",
                       email,
                       token: magicCode,
                     });
+                    if (error) {
+                      toast({
+                        description: error.message,
+                        status: "error",
+                        duration: 3000,
+                        isClosable: true,
+                      });
+                    } else if (data) {
+                      console.log(data.user.email);
+                      determineUser(data.user.email);
+                    }
                   }
                 }}
               >
