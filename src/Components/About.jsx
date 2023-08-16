5b272638e9527727a88fca99d3f08dc39bd8d4e6
galit.moss <galitmoss@me.com>
2023-08-16 12:25:36 +1000

    About page redone

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -5,6 +5,8 @@ import {
   useColorModeValue,
   Flex,
   Stack,
+  Box,
+  Image,
 } from "@chakra-ui/react";
 import { ExternalLinkIcon } from "@chakra-ui/icons";
 
@@ -23,44 +25,24 @@ export default function About() {
         mb={16}
       >
         <Stack
-          align="center"
           color={text}
-          variant="unstyled"
           m="10"
-          fontWeight="semibold"
-          border="transparent"
+          spacing={4}
+          w={{ base: "80%", md: "40%" }}
+          align={["center", "center", "flex-start", "flex-start"]}
         >
           <Text fontSize="30px" p="7px">
-            What is BRAD
+            What is BRAD?
           </Text>
-          <Text textAlign="center" fontSize="20px" p="7px">
+          <Text fontSize="20px" p="7px">
             BRAD stands for Banish Recycling And Disposal Program, started by
             Banish a company who sell sustainable items as well as share
             education on sustainability. Everyday Aussies can send in their hard
             to recycle items, and the team of volunteers will sort and send them
             through to different waste streams.
             <br />
-            For more info on the amazing work Banish do, please visit their
-            website{" "}
-            <Link href="https://banish.com.au" isExternal>
-              here
-              <ExternalLinkIcon mx="5px" />
-            </Link>
           </Text>
-          <Text fontSize="30px" p="7px">
-            How do I play 'Can I BRAD this?'
-          </Text>
-          <Text textAlign="center" fontSize="20px" p="7px">
-            Aimed at people new to the BRAD program or those who want to test &
-            refresh their memory, learn what can and can't be recycled by BRAD
-            through a fun game. A randomly picked item will show up and you need
-            to decide if it can or can't be recycled by the BRAD program. Select
-            your choice by clicking either the 'yes' or 'no' buttons. If you're
-            right, you'll get 1 point! And if you're wrong, your score doesn't
-            change. After each guess, a new option will render for you to
-            decide, can you BRAD this?
-          </Text>
-          <Text textAlign="center" fontSize="20px" p="7px">
+          <Text fontSize="20px" p="7px">
             For more info on BRAD{" "}
             <Link
               href="https://banish.com.au/pages/recycling-program"
@@ -70,11 +52,11 @@ export default function About() {
               <ExternalLinkIcon mx="5px" />
             </Link>
           </Text>
-          <Divider bg={text} m="5px" w="60%" />
+          <Divider bg={text} m="5px" />
           <Text fontSize="30px" p="7px">
             About Me
           </Text>
-          <Text textAlign="center" fontSize="20px" p="7px">
+          <Text fontSize="20px" p="7px">
             Hello, I'm Galit! I've been a volunteer with BRAD for over 2 years
             now, and I absolutely love it! We go and sort through parcels,
             allocating different items into their respective waste streams. And
@@ -90,7 +72,33 @@ export default function About() {
             </Link>
           </Text>
         </Stack>
+        <Box
+          w={{ base: "80%", md: "60%", sm: "40%" }}
+          mb={{ base: 12, md: 0 }}
+          align="center"
+        >
+          <Image
+            src="https://i.ibb.co/qkQ5DVH/IMG-1383.jpg"
+            boxSize="60%"
+            rounded="10px"
+            p="5px"
+          />
+        </Box>
       </Flex>
     </div>
   );
 }
+
+// <Text fontSize="30px" p="7px">
+// How do I play 'Can I BRAD this?'
+// </Text>
+// <Text fontSize="20px" p="7px">
+// Aimed at people new to the BRAD program or those who want to test &
+// refresh their memory, learn what can and can't be recycled by BRAD
+// through a fun game. A randomly picked item will show up and you need
+// to decide if it can or can't be recycled by the BRAD program. Select
+// your choice by clicking either the 'yes' or 'no' buttons. If you're
+// right, you'll get 1 point! And if you're wrong, your score doesn't
+// change. After each guess, a new option will render for you to
+// decide, can you BRAD this?
+// </Text>
