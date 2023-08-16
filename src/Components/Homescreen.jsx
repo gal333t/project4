780b65ec59d46132ebcd54a707002e3997ba0a9d
galit.moss <galitmoss@me.com>
2023-08-16 12:36:08 +1000

    changing colours and also changing on toggle

--- src/Components/Homescreen.jsx
+++ src/Components/Homescreen.jsx
@@ -1,16 +1,17 @@
-import { Text, Box, Flex } from "@chakra-ui/react";
+import { Text, Box, Flex, useColorModeValue } from "@chakra-ui/react";
 import Scoreboard from "./Scoreboard";
-import Username from "./Username";
 
 export default function Homescreen() {
+  const text = useColorModeValue("#005829", "#FBB8FC");
+
   return (
     <>
       <Box>
         <Flex alignItems="center" justifyContent="space-between" p="16">
-          <Text fontSize="40px" color="#66a8ba">
+          <Text fontSize="40px" color={text}>
             IMAGE FROM FINALISED GAME HERE
           </Text>
-          <Text fontSize="20px" color="#66a8ba">
+          <Text fontSize="20px" color={text}>
             BRAD is a recycling program started by Banish. To play, all you need
             to do is decide whether or not an item can be recycled by the
             program.
