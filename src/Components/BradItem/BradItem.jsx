4a8345857cd147932af11a318a3ae88d283d00e4
galit.moss <galitmoss@me.com>
2023-08-22 18:02:52 +1000

    moving Modal from BradItem to it's own component to clean up per feedback on my project

--- src/Components/BradItem/BradItem.jsx
+++ src/Components/BradItem/BradItem.jsx
@@ -11,18 +11,10 @@ import {
   Heading,
   Stack,
   useColorModeValue,
-  IconButton,
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalBody,
-  ModalCloseButton,
-  useDisclosure,
 } from "@chakra-ui/react";
-import { InfoIcon } from "@chakra-ui/icons";
 import { SessionContext } from "../SessionContext";
 import { useContext } from "react";
+import HowTo from "../HowTo";
 
 export default function BradItem() {
   const [bradItems, setBradItems] = useState([]);
@@ -30,7 +22,6 @@ export default function BradItem() {
   const [userAnswer, setUserAnswer] = useState();
   const { username, userScore, setUserScore } = useContext(SessionContext);
   const darkGreen = useColorModeValue("#005929", "#005929");
-  const { isOpen, onOpen, onClose } = useDisclosure();
 
   async function getBradItems() {
     let { data } = await supabase.rpc("random_image");
@@ -93,43 +84,8 @@ export default function BradItem() {
                   borderColor: "#05AA6B",
                 }}
               >
-                <IconButton
-                  color={darkGreen}
-                  bg="white"
-                  _hover={{ bg: "white" }}
-                >
-                  <InfoIcon boxSize={7} onClick={onOpen}></InfoIcon>
-                </IconButton>
-                <Modal isOpen={isOpen} onClose={onClose} p="5">
-                  <ModalOverlay />
-                  <ModalContent>
-                    <ModalHeader m="3px" textAlign="center">
-                      How to play Can I BRAD This?
-                    </ModalHeader>
-                    <ModalBody textAlign="center" mb="15px" fontSize="18px">
-                      A randomly picked item will show up and you need to decide
-                      if it can or can't be recycled by the BRAD program. Select
-                      your choice by clicking either the 'yes' or 'no' buttons.
-                      If you're right, you'll get 1 point! And if you're wrong,
-                      your score doesn't change. If you're logged in, your score
-                      will also be saved to the scoreboard. After each guess,
-                      another option will render for you to decide, can you BRAD
-                      this?
-                      <br />
-                      For more info on what can and can't be recycled, please
-                      visit{" "}
-                      <a
-                        href="https://banish.com.au/pages/recycling-program"
-                        target="_blank"
-                        className="link"
-                      >
-                        Banish.
-                      </a>
-                    </ModalBody>
-                    <ModalCloseButton />
-                  </ModalContent>
-                </Modal>
                 <CardBody align="center">
+                  <HowTo />
                   <Image
                     boxSize="250px"
                     src={brad.imgurl}
---
+++ src/Components/HowTo.jsx
@@ -0,0 +1,52 @@
+import {
+  IconButton,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalCloseButton,
+  useDisclosure,
+  useColorModeValue,
+} from "@chakra-ui/react";
+import { InfoIcon } from "@chakra-ui/icons";
+
+export default function HowTo() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const darkGreen = useColorModeValue("#005929", "#005929");
+
+  return (
+    <>
+      <IconButton color={darkGreen} bg="white" _hover={{ bg: "white" }}>
+        <InfoIcon boxSize={7} onClick={onOpen}></InfoIcon>
+      </IconButton>
+      <Modal isOpen={isOpen} onClose={onClose} p="5">
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader m="3px" textAlign="center">
+            How to play Can I BRAD This?
+          </ModalHeader>
+          <ModalBody textAlign="center" mb="15px" fontSize="18px">
+            A randomly picked item will show up and you need to decide if it can
+            or can't be recycled by the BRAD program. Select your choice by
+            clicking either the 'yes' or 'no' buttons. If you're right, you'll
+            get 1 point! And if you're wrong, your score doesn't change. If
+            you're logged in, your score will also be saved to the scoreboard.
+            After each guess, another option will render for you to decide, can
+            you BRAD this?
+            <br />
+            For more info on what can and can't be recycled, please visit{" "}
+            <a
+              href="https://banish.com.au/pages/recycling-program"
+              target="_blank"
+              className="link"
+            >
+              Banish.
+            </a>
+          </ModalBody>
+          <ModalCloseButton />
+        </ModalContent>
+      </Modal>
+    </>
+  );
+}
